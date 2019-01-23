80e32f2ed775d9bd2d093abc5554a6d664273a36
Andre Simon <as@andre-simon.de>
2019-01-23 23:18:44 +0100

    fixed CI script #3

--- src/ci_test.sh
+++ src/ci_test.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
+# cwd: unpacked tarball dir 
 echo "Starting CI verification script"
 
-cd ~
 git clone https://gitlab.com/tajmone/highlight-test-suite/
 
 #OUTPUT=`./src/highlight --list-scripts=langs`
 HL_BIN=./src/highlight
-TEST_DIR=~/highlight-test-suite
+TEST_DIR=./highlight-test-suite
 
 ${HL_BIN} --list-scripts=langs
 
